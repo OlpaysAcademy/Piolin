1584dc8b7cbacbdc47a46a12a17d68edf2055b1f
JulianMayorga <mayorga.julian@gmail.com>
2016-09-21 18:36:28 -0300

    Integrate redux-little-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
+import { Button } from 'react-toolbox/lib/button';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { createStoreWithRouter, RouterProvider, RelativeFragment as Fragment } from 'redux-little-router';
+
+import app from './reducers';
 import logo from './logo.svg';
 import style from './App.scss';
-import { Button } from 'react-toolbox/lib/button';
+
+const routes = {
+  '/': {
+    title: 'Home'
+  },
+  '/login': {
+    title: 'Login'
+  },
+  '/tweets': {
+    title: 'Tweets'
+  }
+};
+
+let store = createStore(
+  app, // reducer
+  {}, // initial state
+  createStoreWithRouter({
+    routes,
+    pathname: '/login'
+  })
+);
+
+const Gilada = ({ router }) =>
+  <Fragment forRoute={'/'}>
+    <div className={style.App} >
+      <div className={style.header}>
+        <img className={style.logo} src={logo} alt="logo" />
+        <h2>{router.result.title}</h2>
+      </div>
+    </div>
+    <Fragment forRoute={'/login'}>
+      <Button onClick={() => store.dispatch({ type: 'ROUTER_PUSH', payload: '/tweets' })}>
+        Log in
+      </Button>
+    </Fragment>
+    <Fragment forRoute={'/tweets'}>
+      <Button onClick={() => store.dispatch({ type: 'ROUTER_PUSH', payload: '/login' })}>
+        Log out
+      </Button>
+    </Fragment>
+  </Fragment>
 
 class App extends Component {
   render() {
     return (
-      <div className={style.App} >
-        <div className={style.header}>
-          <img className={style.logo} src={logo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={style.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <Button label="Gato el que lee" primary />
-        </p>
-      </div>
+      <Provider store={store}>
+        <RouterProvider store={store}>
+          <Gilada />
+        </RouterProvider>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+export default function app(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
